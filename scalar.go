1904c7e843fbac0043e4183ba28c837eb2a80979
Michael Coyne <mjc@hey.com>
2024-01-03 20:41:59 -0500

    consolidate Scalar types to one file and add ScalarFloat type

---
+++ scalar.go
@@ -0,0 +1,325 @@
+package kredis
+
+import (
+	"fmt"
+	"time"
+)
+
+/* Bool Scalar type */
+
+type ScalarBool struct{ Proxy }
+
+func NewBool(key string, opts ...ProxyOption) (*ScalarBool, error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScalarBool{Proxy: *proxy}, err
+}
+
+func NewBoolWithDefault(key string, defaultValue bool, opts ...ProxyOption) (s *ScalarBool, err error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return
+	}
+
+	s = &ScalarBool{Proxy: *proxy}
+	err = proxy.watch(func() error {
+		return s.SetValue(defaultValue)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (s *ScalarBool) Value() bool {
+	val, err := s.ValueResult()
+	if err != nil {
+		return false
+	}
+
+	return *val
+}
+
+func (s *ScalarBool) ValueResult() (*bool, error) {
+	val, err := s.client.Get(s.ctx, s.key).Bool()
+	if err != nil {
+		return nil, err
+	}
+
+	return &val, nil
+}
+
+func (s *ScalarBool) SetValue(v bool) error {
+	return s.client.Set(s.ctx, s.key, fmt.Sprintf("%v", v), s.expiresIn).Err()
+}
+
+/* Integer Scalar type */
+
+type ScalarInteger struct{ Proxy }
+
+func NewInteger(key string, opts ...ProxyOption) (*ScalarInteger, error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScalarInteger{Proxy: *proxy}, err
+}
+
+func NewIntegerWithDefault(key string, defaultValue int, opts ...ProxyOption) (s *ScalarInteger, err error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return
+	}
+
+	s = &ScalarInteger{Proxy: *proxy}
+	err = proxy.watch(func() error {
+		return s.SetValue(defaultValue)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (s *ScalarInteger) Value() int {
+	val, err := s.ValueResult()
+	if err != nil || val == nil {
+		return 0
+	}
+
+	return *val
+}
+
+func (s *ScalarInteger) ValueResult() (*int, error) {
+	val, err := s.client.Get(s.ctx, s.key).Int()
+	if err != nil {
+		return nil, err
+	}
+
+	return &val, nil
+}
+
+func (s *ScalarInteger) SetValue(v int) error {
+	return s.client.Set(s.ctx, s.key, fmt.Sprintf("%d", v), s.expiresIn).Err()
+}
+
+/* Float Scalar type */
+
+type ScalarFloat struct{ Proxy }
+
+func NewFloat(key string, opts ...ProxyOption) (*ScalarFloat, error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScalarFloat{Proxy: *proxy}, err
+}
+
+func NewFloatWithDefault(key string, defaultValue float64, opts ...ProxyOption) (s *ScalarFloat, err error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return
+	}
+
+	s = &ScalarFloat{Proxy: *proxy}
+	err = proxy.watch(func() error {
+		return s.SetValue(defaultValue)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (s *ScalarFloat) Value() float64 {
+	val, err := s.ValueResult()
+
+	if err != nil || val == nil {
+		return 0
+	}
+
+	return *val
+}
+
+func (s *ScalarFloat) ValueResult() (*float64, error) {
+	val, err := s.client.Get(s.ctx, s.key).Float64()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &val, nil
+}
+
+func (s *ScalarFloat) SetValue(v float64) error {
+	return s.client.Set(s.ctx, s.key, fmt.Sprintf("%f", v), s.expiresIn).Err()
+}
+
+/* String Scalar type */
+
+type ScalarString struct{ Proxy }
+
+func NewString(key string, opts ...ProxyOption) (*ScalarString, error) {
+	proxy, err := NewProxy(key, opts...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScalarString{Proxy: *proxy}, err
+}
+
+func NewStringWithDefault(key string, defaultValue string, opts ...ProxyOption) (s *ScalarString, err error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return
+	}
+
+	s = &ScalarString{Proxy: *proxy}
+	err = proxy.watch(func() error {
+		return s.SetValue(defaultValue)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (s *ScalarString) Value() string {
+	val, err := s.ValueResult()
+	if err != nil || val == nil {
+		return ""
+	}
+
+	return *val
+}
+
+func (s *ScalarString) ValueResult() (*string, error) {
+	val, err := s.client.Get(s.ctx, s.key).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return &val, nil
+}
+
+func (s *ScalarString) SetValue(v string) error {
+	return s.client.Set(s.ctx, s.key, v, s.expiresIn).Err()
+}
+
+/* Time Scalar type */
+
+type ScalarTime struct{ Proxy }
+
+func NewTime(key string, opts ...ProxyOption) (*ScalarTime, error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScalarTime{Proxy: *proxy}, nil
+}
+
+func NewTimeWithDefault(key string, defaultValue time.Time, opts ...ProxyOption) (s *ScalarTime, err error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return
+	}
+
+	s = &ScalarTime{Proxy: *proxy}
+	err = proxy.watch(func() error {
+		return s.SetValue(defaultValue)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (s *ScalarTime) Value() time.Time {
+	val, err := s.ValueResult()
+	if err != nil || val == nil {
+		return time.Time{} // empty value
+	}
+
+	return *val
+}
+
+func (s *ScalarTime) ValueResult() (*time.Time, error) {
+	time, err := s.client.Get(s.ctx, s.key).Time()
+	if err != nil {
+		return nil, err
+	}
+
+	return &time, nil
+}
+
+func (s *ScalarTime) SetValue(v time.Time) error {
+	s.client.Set(s.ctx, s.key, v.Format(time.RFC3339Nano), s.expiresIn)
+
+	return nil
+}
+
+/* JSON Scalar type */
+
+type ScalarJSON struct{ Proxy }
+
+func NewJSON(key string, opts ...ProxyOption) (*ScalarJSON, error) {
+	proxy, err := NewProxy(key, opts...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScalarJSON{Proxy: *proxy}, nil
+}
+
+func NewJSONWithDefault(key string, defaultValue *kredisJSON, opts ...ProxyOption) (s *ScalarJSON, err error) {
+	proxy, err := NewProxy(key, opts...)
+	if err != nil {
+		return
+	}
+
+	s = &ScalarJSON{Proxy: *proxy}
+	err = proxy.watch(func() error {
+		return s.SetValue(defaultValue)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+// TODO should this be returning a pointer instead struct value itself??
+func (s *ScalarJSON) Value() kredisJSON {
+	val, err := s.ValueResult()
+	if err != nil || val == nil {
+		return kredisJSON{}
+	}
+
+	return *val
+}
+
+func (s *ScalarJSON) ValueResult() (*kredisJSON, error) {
+	val, err := s.client.Get(s.ctx, s.key).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return NewKredisJSON(val), nil
+}
+
+func (s *ScalarJSON) SetValue(v *kredisJSON) error {
+	return s.client.Set(s.ctx, s.key, string(v.s), s.expiresIn).Err()
+}
---
+++ scalar_test.go
@@ -0,0 +1,311 @@
+package kredis
+
+import (
+	"testing"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+func (s *KredisTestSuite) TestScalarBoolWithUnknownConnection() {
+	_, e := NewBool("foo", WithConfigName("unknown"))
+
+	s.Error(e)
+}
+
+func (s *KredisTestSuite) TestScalarBool() {
+	k, e := NewBool("foo")
+
+	s.NoError(e)
+	s.Empty(k.Value())
+
+	e = k.SetValue(false)
+
+	s.NoError(e)
+	s.False(k.Value())
+
+	e = k.SetValue(true)
+
+	s.NoError(e)
+	s.True(k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarBoolWithDefaultValue() {
+	k, e := NewBoolWithDefault("foo", true)
+
+	s.NoError(e)
+	s.True(k.Value())
+
+	k, e = NewBoolWithDefault("bar", false)
+	s.NoError(e)
+	s.False(k.Value())
+
+	k, e = NewBoolWithDefault("bar", true)
+	s.NoError(e)
+	s.False(k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarBoolBadConnection() {
+	k, _ := NewBool("foo", WithConfigName("badconn"))
+	_, e := k.ValueResult()
+
+	s.Error(e)
+	s.Empty(k.Value())
+
+	k, e = NewBoolWithDefault("foo", true, WithConfigName("badconn"))
+	s.Error(e)
+	s.Nil(k)
+}
+
+func (s *KredisTestSuite) TestScalarBoolExpiresIn() {
+	k, _ := NewBool("foo", WithExpiry("1ms"))
+
+	e := k.SetValue(true)
+	s.NoError(e)
+
+	time.Sleep(5 * time.Millisecond)
+
+	v, e := k.ValueResult()
+	s.Equal(redis.Nil, e)
+	s.Nil(v)
+}
+
+func (s *KredisTestSuite) TestNewString() {
+	k, e := NewString("foo")
+
+	s.NoError(e)
+	s.Empty(k.Value())
+
+	e = k.SetValue("bar")
+
+	s.NoError(e)
+	s.Equal("bar", k.Value())
+}
+
+func (s *KredisTestSuite) TestNewStringWithDefaultValue() {
+	k, e := NewStringWithDefault("foo", "bar")
+
+	s.NoError(e)
+	s.Equal("bar", k.Value())
+
+	k, e = NewStringWithDefault("bar", "baz")
+	s.NoError(e)
+	s.Equal("baz", k.Value())
+
+	k, e = NewStringWithDefault("bar", "quix")
+	s.NoError(e)
+	s.Equal("baz", k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarStringExpiresIn() {
+	k, _ := NewString("foo", WithExpiry("1ms"))
+
+	e := k.SetValue("bar")
+	s.NoError(e)
+
+	time.Sleep(5 * time.Millisecond)
+
+	v, e := k.ValueResult()
+	s.Equal(redis.Nil, e)
+	s.Nil(v)
+}
+
+func (s *KredisTestSuite) TestScalarIntegerWithUnknownConnection() {
+	_, e := NewInteger("foo", WithConfigName("unknown"))
+
+	s.Error(e)
+}
+
+func (s *KredisTestSuite) TestScalarInteger() {
+	k, e := NewInteger("foo")
+
+	s.NoError(e)
+	s.Empty(k.Value())
+
+	e = k.SetValue(1234)
+
+	s.NoError(e)
+	s.Equal(1234, k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarIntegerWithDefaultValue() {
+	k, e := NewIntegerWithDefault("foo", 5678)
+
+	s.NoError(e)
+	s.Equal(5678, k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarIntegerBadConnection() {
+	k, _ := NewInteger("foo", WithConfigName("badconn"))
+	_, e := k.ValueResult()
+
+	s.Error(e)
+	s.Empty(k.Value())
+
+	k, e = NewIntegerWithDefault("foo", -1, WithConfigName("badconn"))
+	s.Error(e)
+	s.Nil(k)
+}
+
+func (s *KredisTestSuite) TestScalarIntegerExpiresIn() {
+	k, _ := NewInteger("foo", WithExpiry("1ms"))
+
+	e := k.SetValue(828)
+	s.NoError(e)
+
+	time.Sleep(5 * time.Millisecond)
+
+	v, e := k.ValueResult()
+	s.Equal(redis.Nil, e)
+	s.Nil(v)
+}
+
+func (s *KredisTestSuite) TestScalarFloatWithUnknownConnection() {
+	_, e := NewFloat("foo", WithConfigName("unknown"))
+	s.Error(e)
+}
+
+func (s *KredisTestSuite) TestScalarFloat() {
+	k, e := NewFloat("foo")
+	s.NoError(e)
+	s.Empty(k.Value())
+
+	e = k.SetValue(12.34)
+	s.NoError(e)
+	s.Equal(float64(12.34), k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarFloatWithDefaultValue() {
+	k, e := NewFloatWithDefault("foo", 56.78)
+
+	s.NoError(e)
+	s.Equal(float64(56.78), k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarFloatBadConnection() {
+	k, _ := NewFloat("foo", WithConfigName("badconn"))
+	_, e := k.ValueResult()
+
+	s.Error(e)
+	s.Empty(k.Value())
+
+	k, e = NewFloatWithDefault("foo", -1, WithConfigName("badconn"))
+	s.Error(e)
+	s.Nil(k)
+}
+
+func (s *KredisTestSuite) TestScalarFloatExpiresIn() {
+	k, _ := NewFloat("foo", WithExpiry("1ms"))
+
+	e := k.SetValue(828)
+	s.NoError(e)
+
+	time.Sleep(5 * time.Millisecond)
+
+	v, e := k.ValueResult()
+	s.Equal(redis.Nil, e)
+	s.Nil(v)
+}
+
+func (s *KredisTestSuite) TestNewTime() {
+	k, e := NewTime("t")
+
+	s.NoError(e)
+	s.Empty(k.Value())
+
+	now := time.Now()
+
+	e = k.SetValue(now)
+	s.NoError(e)
+
+	// The canonical way to strip a monotonic clock reading is to use t =
+	// t.Round(0).
+	s.Equal(now.Round(0), k.Value().Local())
+}
+
+func (s *KredisTestSuite) TestNewTimeWithDefaultValue() {
+	yesterday := time.Now().Add(-24 * time.Hour)
+	now := time.Now()
+
+	k, e := NewTimeWithDefault("t1", yesterday)
+
+	s.NoError(e)
+	s.Equal(yesterday.Round(0), k.Value().Local())
+
+	k, e = NewTimeWithDefault("t2", now)
+	s.NoError(e)
+	s.Equal(now.Round(0), k.Value().Local())
+
+	k, e = NewTimeWithDefault("t2", yesterday)
+	s.NoError(e)
+	s.Equal(now.Round(0), k.Value().Local())
+}
+
+func (s *KredisTestSuite) TestScalarTimeExpiresIn() {
+	k, _ := NewTime("foo", WithExpiry("1ms"))
+
+	e := k.SetValue(time.Now())
+	s.NoError(e)
+
+	time.Sleep(5 * time.Millisecond)
+
+	v, e := k.ValueResult()
+	s.Equal(redis.Nil, e)
+	s.Nil(v)
+}
+
+func (s *KredisTestSuite) TestNewJSON() {
+	kjson := NewKredisJSON(`{"key":"value"}`)
+	k, e := NewJSON("json")
+
+	s.NoError(e)
+	s.Empty(k.Value())
+
+	e = k.SetValue(kjson)
+
+	s.NoError(e)
+	s.Equal(*kjson, k.Value())
+}
+
+func (s *KredisTestSuite) TestNewJSONWithDefaultValue() {
+	kjson := NewKredisJSON(`{"key":"default"}`)
+	kjson2 := NewKredisJSON(`{"k2":"v2"}`)
+	k, e := NewJSONWithDefault("foo", kjson)
+
+	s.NoError(e)
+	s.Equal(*kjson, k.Value())
+
+	k, e = NewJSONWithDefault("bar", kjson2)
+	s.NoError(e)
+	s.Equal(*kjson2, k.Value())
+
+	k, e = NewJSONWithDefault("bar", kjson)
+	s.NoError(e)
+	s.Equal(*kjson2, k.Value())
+}
+
+func (s *KredisTestSuite) TestScalarJSONExpiresIn() {
+	k, _ := NewJSON("foo", WithExpiry("1ms"))
+
+	e := k.SetValue(NewKredisJSON(`[1]`))
+	s.NoError(e)
+
+	time.Sleep(5 * time.Millisecond)
+
+	v, e := k.ValueResult()
+	s.Equal(redis.Nil, e)
+	s.Nil(v)
+}
+
+func TestScalarJSONUnmarshal(t *testing.T) {
+	var data interface{}
+
+	kj := NewKredisJSON("[1]")
+	err := kj.Unmarshal(&data)
+
+	nums := data.([]interface{})
+
+	assert.NoError(t, err)
+	assert.Contains(t, nums, 1.0)
+}
